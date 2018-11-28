6893b5ce7a472c7f1794903e9c55a6db2d4fd56d
Mathias Bærentsen <math4249@edu.eal.dk>
2018-11-28 12:33:10 +0100

    Add project files.

---
+++ Lagersystem/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagersystem
+{
+    public class Menu
+    {
+        public void ShowMenu()
+        {
+            bool menuRunning = true;
+            do
+            {
+                menuStructure();
+                Console.Write("Indtast menu punkt: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        menuRunning = false;
+                        break;
+                    case "1":
+
+                        break;
+                    default:
+                        Console.WriteLine("Menupunktet findes ikke, prøv igen.");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (menuRunning);
+        }
+
+        public void menuStructure()
+        {
+            List<string> menu = new List<string>()
+            {
+                "punkt1"
+            };
+
+            Console.Clear();
+            Console.Write("╔");
+            Console.Write(new String('═', Console.WindowWidth - 3));
+            Console.WriteLine("╗");
+            for (int i = 0; i < menu.Count; i++)
+            {
+                MenuStructureSetup(i + 1 + ". " + menu[i]);
+            }
+
+            MenuStructureSetup("0. Exit");
+            Console.Write("╚");
+            Console.Write(new String('═', Console.WindowWidth - 3));
+            Console.WriteLine("╝");
+        }
+        
+        public void MenuStructureSetup(string menuPoint)
+        {
+            string text = "║  " + menuPoint;
+            StringBuilder spaces = new StringBuilder();
+            for (int i = 0; i < (Console.WindowWidth - text.Length - 2); i++)
+            {
+                spaces.Append(" ");
+            }
+            Console.WriteLine(text + spaces + "║");
+        }
+
+        //Menu metoder
+        //private void MetodeNavn()
+        //{
+        //    Console.Write("Text: ");
+        //    string inputText = Console.ReadLine();
+        //    Console.Clear();
+        //    //Kald til metoden
+        //}
+    }
+}
